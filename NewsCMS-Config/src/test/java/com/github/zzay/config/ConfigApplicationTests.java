5f17fdecce3128bcc78e12d44ba02635db38b40e
zzay0921 <1015864587@qq.com>
2022-04-23 14:35:53 +0800

    update "config-service"

---
+++ NewsCMS-Config/src/test/java/com/github/zzay/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zzay.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
